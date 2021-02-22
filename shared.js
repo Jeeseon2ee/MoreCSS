86b7c63284461b0ad098468ca675b47a12180224
Jeeseon2ee <78467596+Jeeseon2ee@users.noreply.github.com>
2021-02-23 01:47:53 +0900

    Add files via upload
    Day 7
    1. Transition & Animation (@keyframes)
    2. variables (--name)
    3. Vender Prefixes (autoprefixers)
    4. @support () {}
    5. Frameworks (ex. Bootstrap, Tailwind)
    6. Sass (&Scss): nesting, variables, inheritance, mixins,,,

--- shared.js
+++ shared.js
@@ -4,17 +4,21 @@ var modalNoButton = document.querySelector(".modal_action_negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle_button");
 var mobileNav = document.querySelector(".mobile_nav");
+var mainButton = document.querySelector(".main_nav_item_button");
 
 // console.dir(backdrop.style['background-image']);
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function() {
-    modal.style.display = "block";
-    backdrop.style.display = "block";
+    //modal.style.display = "block";
+    //backdrop.style.display = "block";
     // modal.className = 'open'; // This will actually overwrite the complete class list
-    //modal.classList.add("open");
-    //backdrop.classList.add("open");
+    modal.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function() {
+      backdrop.classList.add("open");
+    }, 10)
   });
 }
 
@@ -29,17 +33,35 @@ if (modalNoButton) {
 }
 
 function closeModal() {
-  backdrop.style.display = "none";
-  modal.style.display = "none";
+  //backdrop.style.display = "none";
+  //modal.style.display = "none";
   if (modal) {
     modal.classList.remove("open");
   }
   backdrop.classList.remove("open");
+  setTimeout(function() {
+    backdrop.style.display = "none";
+  }, 200);
 }
 
 toggleButton.addEventListener("click", function() {
   // mobileNav.style.display = 'block';
   // backdrop.style.display = 'block';
   mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(function() {
+    backdrop.classList.add("open");
+  }, 10)
 });
+
+mainButton.addEventListener("animationstart"), function(event) {
+  console.log("Animation started", event);
+}
+
+mainButton.addEventListener("animationend"), function(event) {
+  console.log("Animation ended", event);
+}
+
+mainButton.addEventListener("animationiteration"), function(event) {
+  console.log("Animation iteration", event);
+}
